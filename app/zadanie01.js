8855053f4c04337adc658b05b67ddb2c5aecd07c
jakub <gaboryjakub@gmail.com>
2018-02-05 23:39:46 +0100

    5th day tasks

--- app/zadanie01.js
+++ app/zadanie01.js
@@ -1,1 +1,16 @@
-//Twój kod
+const express = require('express');
+const PORT = 3000;
+const app =express();
+
+app.get('/:num1/:num2', (req, res) => {
+    let num1 =req.params.num1;
+    let num2 =req.params.num2;
+
+   let result = parseInt(num1) + parseInt(num2);
+   res.send(`${num1} + ${num2} = ${result}`);
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
--- app/zadanie02.js
+++ app/zadanie02.js
@@ -1,1 +1,46 @@
-//Twój kod
+const express = require('express');
+const PORT = 3000;
+const app = express();
+const fs = require('fs');
+const file = './name.txt';
+
+app.get('/name/set/:name', (req, res) => {
+    let name = req.params.name;
+    fs.writeFile(file, name, err => {
+       if(err !== null){
+           console.log(err);
+           res.send(`There was an error while saving a file`);
+           return false;
+       }
+       res.send(name);
+    });
+});
+
+
+
+app.get('/name/show', (req, res) => {
+    fs.readFile(file, 'utf-8', (err,data) => {
+        if(err !== null){
+            res.send(`There was an error while reading file ${file}: ${err}`);
+            return false;
+        }
+        res.send(data);
+    });
+});
+
+
+app.get('/name/check', (req, res) => {
+    fs.exists(file, exist =>{
+        if(exist){
+            res.send('Name was already saved');
+        }else{
+            res.send('Name wasn\'t saved yet');
+        }
+
+    })
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+});
